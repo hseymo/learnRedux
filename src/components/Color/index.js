c70955b5e088be24cf835467807657afbb6e48cf
Haley Seymour <haleycseymour@comcast.net>
2022-09-06 12:05:44 -0700

    basic react app and 2 components with styling differential

---
+++ src/components/Color/index.js
@@ -0,0 +1,11 @@
+// import './style.css';
+
+function Color() {
+  return (
+    <div className="Color card">
+      <h1>Hello I'm The Color Component</h1>
+    </div>
+  );
+}
+
+export default Color;
---
+++ src/components/Count/index.js
@@ -0,0 +1,11 @@
+// import './style.css';
+
+function Count() {
+  return (
+    <div className="Count card">
+      <h1>Hello I'm The Count Component</h1>
+    </div>
+  );
+}
+
+export default Count;
